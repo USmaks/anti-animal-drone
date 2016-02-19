cad3c04a6172af0ff1a5377366d6d07c29affa2d
usmaks <anoly.konz.0099@gmail.com>
2016-02-20 08:24:10 +0900

    Modify server and ex.

---
+++ ex-serialport.js
@@ -0,0 +1,26 @@
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+var sp = new SerialPort('COM5', {
+	baudrate: 57600,
+	dataBits: 8,
+	parity: 'none',
+	stopBits: 1,
+	flowControl: false
+//	parser: serialport.parsers.readline("\n")
+});
+
+sp.on('open', function (){
+	console.log('open');
+	sp.on('data', function (data){
+		var buffer = new Buffer(data, 'utf8');
+		try {
+//			console.log('data received: ' + buffer);
+			sys.puts('here: ' + data);
+		} catch (e) {
+			console.log('Error');
+			return;
+		}
+	});
+});
+
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 http.listen(PORT, function () {
-  console.log('Server started!');
+  console.log('Server started on ' + PORT + 'port!');
 });
 
 io.on('connection', function (socket) {
@@ -24,16 +24,63 @@ io.on('connection', function (socket) {
 /////////////////////////////////////// Server //
 
 // Arduino //////////////////////////////////////
-var A0 = 0;
 var five = require('johnny-five'),
     temporal = require('temporal'),
     board;
 
-var board = new five.Board();
+var five = require("johnny-five"),
+  temporal = require("temporal"),
+  board;
 
-board.on('ready', function () {
-  this.pinMode(0, five.Pin.ANALOG);
-  A0 = this.analogRead(9);
+board = new five.Board();
+
+board.on("ready", function(){
+  console.log("CONNECTED");
+
+//  this.pinMode(0, five.Pin.ANALOG);
+//	var A0 = this.analogRead(9);
+//	console.log(A0);
+
+  var servoRotateR = new five.Servo(13);
+  var servoRotateL = new five.Servo(12);
+  var servoSlantR = new five.Servo(11);
+  var servoSlantL = new five.Servo(10);
+
+//  this.pinMode(1, five.Pin.ANALOG);
+  temporal.queue([{
+    // Initial servo motor state
+    delay: 0,
+    task: function(){
+      servoRotateR.min();
+      servoRotateL.min();
+      servoSlantR.min();
+      servoSlantL.min();
+    }
+  },{
+    // Flying Start
+    delay: 5000, // waiting time about 5 seconds
+    task: function(){
+      servoSlantR.center();
+      servoSlantL.center();
+    }
+  },{
+    delay: 10000, // 10 seconds
+    task: function(){
+      servo.stop();
+    }
+  },{
+    // Keeping the altitude
+    delay: 1000,
+    task: function(){
+      servoSlantR.min();
+      servoSlantL.min();
+    }
+  },{
+    // Moving start
+    delay: 2000,
+    task: function(){
+      servo.to(0);
+    }
+  }]);
 });
-
 ////////////////////////////////////// Arduino //
